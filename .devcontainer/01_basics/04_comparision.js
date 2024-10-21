e5e81bd80edd27e1701e2a50f20c5dd54bf20908
shubham srivastava <shubhamsrivastava17494@gmail.com>
2024-10-21 05:48:28 +0000

    comaperision

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,14 @@
+//console.log("2"> 0);
+//console.log("02" > 0);
+
+console.log(null== 0);
+console.log(null>= 0);
+
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+//===
+
+console.log("2"===2);
