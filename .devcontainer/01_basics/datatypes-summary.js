c61b3044c355d07086c899f6ecd5077048eb1a95
shubham srivastava <shubhamsrivastava17494@gmail.com>
2024-10-21 06:18:13 +0000

    memory

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+//********************************* 
+
+//STack(premitive), Heap(Non- Primitive)
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+     email : " user@google.com",
+     upi: " usert@ybl"
+}
+
+let userTwo = userOne 
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
